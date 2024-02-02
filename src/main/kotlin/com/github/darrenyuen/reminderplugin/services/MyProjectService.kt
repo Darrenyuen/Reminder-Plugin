aa922c07d7bddfcac8809520ddc55d61f53ed242
GitHub Action <action@github.com>
2024-02-02 12:06:17 +0000

    Template cleanup

--- src/main/kotlin/com/github/darrenyuen/reminderplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/darrenyuen/reminderplugin/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.darrenyuen.reminderplugin.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.darrenyuen.reminderplugin.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
